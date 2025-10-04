6cb4ff4649806cadf81a204d7df5765d53f92807
Dzaky <jekdar96@gmail.com>
2025-10-04 18:04:34 +0300

    gsap

--- src/components/daftarSayaComp/conDaftarSaya.jsx
+++ src/components/daftarSayaComp/conDaftarSaya.jsx
@@ -1,19 +1,34 @@
 import { useEffect } from "react"
+import { useRef } from "react"
 import usedaftarsaya from "../../store/useDaftarSaya"
 import useModal from "../../store/useModal"
 import FilmModal from "../filmModal"
 import LabelEpisode from "../labelEpisode"
+import gsap from "gsap"
+
 
 const ConDaftarSaya = () => {
     const listDaftarSaya = usedaftarsaya((state)=>(state.listDaftarSaya))
     const isOpen = useModal((state)=> (state.isOpen))
     const setIsOpen = useModal((state)=> (state.setIsOpen))
+    const itemRef = useRef([])
+
+    useEffect(()=> {
+      gsap.to(itemRef.current,{
+        y: 50,
+        opacity: 1,
+        stagger: 0.2,
+        duration: 0.8,
+        ease: 'power3.in'
+      })
+    })
 
    
     return(
         <div className="w-full flex flex-row flex-wrap gap-x-2 md:gap-x-4 gap-y-8 px-auto py-8 md:px-8 md:py-12 justify-center items-center">
           {listDaftarSaya.map((film,index) => (
                  <div key={index}
+                 ref={(el)=> itemRef.current[index] = el}
                   className="shrink-0 relative h-72 w-52 md:h-80 md:w-56 hover:scale-95"
                   onClick={() => {
                     setIsOpen(film)
